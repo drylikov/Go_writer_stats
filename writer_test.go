4012985aeff1c635e8801380effc9c48edeeff70
drylikov <denis.rylikov@protonmail.com>
2026-04-30 10:12:10 +0300

    Initial commit: Go_writer_stats

---
+++ writer_test.go
@@ -0,0 +1,28 @@
+
+package writer
+
+import (
+	"io/ioutil"
+	"testing"
+
+)
+
+func TestWrites(t *testing.T) {
+	w := New(ioutil.Discard)
+
+	assert.Equal(t, uint64(0), w.Writes())
+	w.Write([]byte("hello"))
+	assert.Equal(t, uint64(1), w.Writes())
+	w.Write([]byte("hello"))
+	assert.Equal(t, uint64(2), w.Writes())
+}
+
+func TestBytes(t *testing.T) {
+	w := New(ioutil.Discard)
+
+	assert.Equal(t, uint64(0), w.Bytes())
+	w.Write([]byte("hello"))
+	assert.Equal(t, uint64(5), w.Bytes())
+	w.Write([]byte("hello"))
+	assert.Equal(t, uint64(10), w.Bytes())
+}
